c77d927fd9d2982e932950e05374765c98051be3
Shiv Kushwah <shivendrakushwah@gmail.com>
2019-05-21 22:36:51 -0700

    fixed cli and cleaning up code

--- App/App.cpp
+++ App/App.cpp
@@ -1,5 +1,4 @@
 #include <stdio.h>
-//#include <zmq.hpp>
 #include <iostream>
 #include "Enclave_u.h"
 #include "sgx_urts.h"
@@ -8,246 +7,144 @@
 
 #include <unistd.h>
 
-
 /* Global EID shared by multiple threads */
 sgx_enclave_id_t global_eid = 0;
 
 // OCall implementations
-void ocall_print(const char* str) {
+void ocall_print(const char *str)
+{
     printf("%s\n", str);
 }
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
 
-
-    if (initialize_enclave(&global_eid, "enclave.token", "enclave.signed.so") < 0) {
+    if (initialize_enclave(&global_eid, "enclave.token", "enclave.signed.so") < 0)
+    {
         std::cout << "Fail to initialize enclave." << std::endl;
         return 1;
     }
 
-   // zmq::context_t context (1);
-   // zmq::socket_t socket (context, ZMQ_REP);
-   // socket.bind ("tcp://*:5555");
-   // std::cout << "Received Hello" << std::endl;
+    printf("Welcome to the Intel-SGX Password Manager\n");
+    printf("Type \"help\"\n");
 
-    while (true) {
+    while (true)
+    {
 
+        // std::string command;
+        char strCommand[100];
 
+        char str1[100];
+        char str2[100];
+        char str3[100];
+        char str4[100];
+        int i;
+        void *encrypt = malloc(100);
 
-    char str1[100];
-    char str2[100];
-    char str3[100];
-    char str4[100];
-   int i;
-   void* encrypt = malloc(100);
+        printf("Enter a command: ");
 
-   printf( "Enter a command :");
-   // fgets(str, 10, stdin);
-   //  /* remove newline, if present */
-   //  i = strlen(str)-1;
-   //  if( str[ i ] == '\n') 
-   //      str[i] = '\0';
+        // std::getline (std::cin, command);
 
-   scanf("%s %s %s %s", str1, str2, str3, str4);
-   if (strcmp(str1, "create") == 0) {
-    printf("Creating Password Manager");
+        scanf("%[^\n]%*c", strCommand);
+        char* split = strtok(strCommand, " ");
+        strcpy(str1, split);
 
-    //str2 = main keystore password
-    int create_keystore_return;
-    sgx_status_t status = create_keystore(global_eid, &create_keystore_return, str2);
+        if (strcmp(str1, "help") == 0) {
+            printf("Example Usage\n");
+            printf("create MasterPassword\n");
+            printf("add Website WebsitePassword\n");
+            printf("get Website MasterPassword\n");
+            printf("quit\n");
 
-   } else if (strcmp(str1, "add_password") == 0) {
-    printf("Adding Password");
+        } 
+        else if (strcmp(str1, "quit") == 0) 
+        {
+            break;
+        } 
+        else if (strcmp(str1, "create") == 0)
+        {
 
-     //add pass1
-    int add_password_return;
-    // strings are const char* ptrs in modern C/C++ compilers
-    //char password[] = "pass1";
-    //char website[] = "web1";
-
-    //str2 = website
-    //str3 = password
-    sgx_status_t status2 = add_password(global_eid, &add_password_return, str2, str3);
-    printf("add_password returned: %u\n", add_password_return);
+            printf("Creating Password Manager\n");
+            split = strtok(NULL, " ");
+            strcpy(str2, split);
 
 
-   } else if (strcmp(str1, "get_password") == 0) {
-    printf("Getting Password");
+            //str2 = main keystore password
+            int create_keystore_return;
+            sgx_status_t status = create_keystore(global_eid, &create_keystore_return, str2);
+        }
+        else if (strcmp(str1, "add") == 0)
+        {
+            printf("Adding Password\n");
 
-    char get_password_return_str[16];
-    int get_password_return;
-    //str2 = website
-    //str3 = main keystore password
-    sgx_status_t status3 = get_password(global_eid, &get_password_return, str2, get_password_return_str, str3);
-    printf("get_password returned: %u\n", get_password_return);
-    printf("get_password buffer: %s\n", get_password_return_str);
+            int add_password_return;
+            split = strtok(NULL, " ");
+            strcpy(str2, split);
+            split = strtok(NULL, " ");
+            strcpy(str3, split);
 
-   } else if (strcmp(str1, "encrypt") == 0) {
-    printf("Serializing Keystore");
 
-     int encrypt_return;
-    //void* encrypt = malloc(100);
-    //save to file
-    FILE *fp = fopen("encrypt.txt", "w+");
-    
+            //str2 = website
+            //str3 = password
+            sgx_status_t status2 = add_password(global_eid, &add_password_return, str2, str3);
+            printf("add_password returned: %u\n", add_password_return);
+        }
+        else if (strcmp(str1, "get") == 0)
+        {
+            printf("Getting Password");
 
-    sgx_status_t status4 = encrypt_and_serialize_key_store(global_eid, &encrypt_return, encrypt);
-    fprintf (fp, "%s", encrypt);
-    fclose(fp);
-    printf("serialize_key_store returned: %u\n", encrypt_return);
-    printf("serialize_key_store string: %s\n", (char*) encrypt);
-   } else if (strcmp(str1, "decrypt") == 0) {
-    printf("Decrypting and Setting Keystore");
+            char get_password_return_str[16];
+            int get_password_return;
+            split = strtok(NULL, " ");
+            strcpy(str2, split);
+            split = strtok(NULL, " ");
+            strcpy(str3, split);
 
-    //void* encrypt = malloc(100);
-    int encrypt_return;
-    // size_t nread;
 
-    // FILE *file = fopen("encrypt.txt", "r");
-    // if (file) {
-    //     while ((nread = fread(encrypt, 1, sizeof encrypt, file)) > 0)
-    //         fwrite(encrypt, 1, nread, stdout);
-    //     if (ferror(file)) {
-    //         /* deal with error */
-    //     }
-    //     fclose(file);
-    // }
-    printf("encrypted string %s", encrypt);
-    sgx_status_t status5 = decrypt_and_set_key_store(global_eid, &encrypt_return, encrypt);
+            //str2 = website
+            //str3 = main keystore password
+            sgx_status_t status3 = get_password(global_eid, &get_password_return, str2, get_password_return_str, str3);
+            printf("get_password returned: %u\n", get_password_return);
+            printf("get_password buffer: %s\n", get_password_return_str);
+        }
+        else if (strcmp(str1, "encrypt") == 0)
+        {
+            //Serializes keystone (all data along with masterpassword) and saves to file
+            printf("Serializing Keystore");
 
-   }
+            int encrypt_return;
+            FILE *fp = fopen("encrypt.txt", "w+");
 
+            //TODO: Look at git history for working encrypt example, does this version write to file correctly?
+
+            sgx_status_t status4 = encrypt_and_serialize_key_store(global_eid, &encrypt_return, encrypt);
+            fprintf(fp, "%s", encrypt);
+            fclose(fp);
+            printf("serialize_key_store returned: %u\n", encrypt_return);
+            printf("serialize_key_store string: %s\n", (char *)encrypt);
+        }
+        else if (strcmp(str1, "decrypt") == 0)
+        {
+            printf("Decrypting and Setting Keystore");
+
+            //TODO: read from encrypt.txt to set the keystone to all of the old values
+
+            int encrypt_return;
+            // size_t nread;
+
+            // FILE *file = fopen("encrypt.txt", "r");
+            // if (file) {
+            //     while ((nread = fread(encrypt, 1, sizeof encrypt, file)) > 0)
+            //         fwrite(encrypt, 1, nread, stdout);
+            //     if (ferror(file)) {
+            //         /* deal with error */
+            //     }
+            //     fclose(file);
+            // }
+            printf("encrypted string %s", encrypt);
+            sgx_status_t status5 = decrypt_and_set_key_store(global_eid, &encrypt_return, encrypt);
+        }
     }
-
-   
-
-
-  
-    char main_password[] = "password";
-    int create_keystore_return;
-    sgx_status_t status = create_keystore(global_eid, &create_keystore_return, main_password);
-
-
-    // //add pass1
-    int add_password_return;
-    // strings are const char* ptrs in modern C/C++ compilers
-    char password[] = "pass1";
-    char website[] = "web1";
-    sgx_status_t status2 = add_password(global_eid, &add_password_return, website, password);
-    printf("add_password returned: %u\n", add_password_return);
-
-    //serialize the keystore
-    
-    int encrypt_return;
-    void* encrypt = malloc(100);
-
-    sgx_status_t status4 = encrypt_and_serialize_key_store(global_eid, &encrypt_return, encrypt);
-
-    printf("serialize_key_store returned: %u\n", encrypt_return);
-    printf("serialize_key_store string: %s\n", (char*) encrypt);
-
-    
-
-    //add pass2
-    int add_password_return2;
-    char password2[] = "pass2";
-    char website2[] = "web2";
-    //ocall_print(password2);
-    sgx_status_t status22 = add_password(global_eid, &add_password_return2, website2, password2);
-    printf("add_password returned: %u\n", add_password_return2);
-
-    //get pass1
-    char get_password_return_str[16];
-    int get_password_return;
-    sgx_status_t status3 = get_password(global_eid, &get_password_return, website, get_password_return_str, main_password);
-    printf("get_password returned: %u\n", get_password_return);
-    printf("get_password buffer: %s\n", get_password_return_str);
-
-    //get pass2
-    char get_password_return_str2[16];
-    int get_password_return2;
-    sgx_status_t status33 = get_password(global_eid, &get_password_return2, website2, get_password_return_str2, main_password);
-    printf("get_password returned: %u\n", get_password_return2);
-    printf("get_password buffer: %s\n", get_password_return_str2);
-
-    
-
-    //set keystore to only have pass1
-    sgx_status_t status5 = decrypt_and_set_key_store(global_eid, &encrypt_return, encrypt);
-
-
-    //get pass2 (should not be able to find)
-    sgx_status_t status333 = get_password(global_eid, &get_password_return, website2, get_password_return_str, main_password);
-    printf("get_password returned: %u\n", get_password_return);
-    printf("get_password buffer: %s\n", get_password_return_str); 
-
-    //get pass 1 (should be able to find)
-    status3 = get_password(global_eid, &get_password_return, website, get_password_return_str, main_password);
-    printf("get_password returned: %u\n", get_password_return);
-    printf("get_password buffer: %s\n", get_password_return_str);
-
-
-
-
-
-    //char get_password_buffer[16];
-    //int get_password_return;
-    //sgx_status_t status3 = get_password(global_eid, &get_password_return, get_password_buffer, 16);
-    //printf("get_password returned: %u\n", get_password_return);
-    //printf("get_password buffer: %s\n", get_password_buffer);
-
-    
-
-    void* encypted_key_store =  malloc(48);//malloc(numPasswords * sizeof(struct KeyStoreBank)); 
-    uint8_t* rand_vector;
-    //const uint8_t pw;
-
-    //sgx_status_t bro = sgx_read_rand(global_eid, &encrypt_return, rand_vector, sizeof(uint8_t));
-
-    //sgx_rijndael128GCM_encrypt(pw, encrypt, 24, encypted_key_store, rand_vector, sizeof(uint8_t) , NULL, NULL, NULL);
-
-
-
-    std::cout << status2 << std::endl;
-    if (status2 != SGX_SUCCESS) {
-        std::cout << "noob" << std::endl;
-    }
-    std::cout << status3 << std::endl;
-    if (status3 != SGX_SUCCESS) {
-        std::cout << "noob2" << std::endl;
-    }
-
-    //printf("Random number: %d\n", ptr);
-    
-
-    /*
-
-    // Seal the random number
-    size_t sealed_size = sizeof(sgx_sealed_data_t) + sizeof(ptr);
-    uint8_t* sealed_data = (uint8_t*)malloc(sealed_size);
-
-    sgx_status_t ecall_status;
-    status = seal(global_eid, &ecall_status,
-            (uint8_t*)&ptr, sizeof(ptr),
-            (sgx_sealed_data_t*)sealed_data, sealed_size);
-
-    if (!is_ecall_successful(status, "Sealing failed :(", ecall_status)) {
-        return 1;
-    }
-
-    int unsealed;
-    status = unseal(global_eid, &ecall_status,
-            (sgx_sealed_data_t*)sealed_data, sealed_size,
-            (uint8_t*)&unsealed, sizeof(unsealed));
-
-    if (!is_ecall_successful(status, "Unsealing failed :(", ecall_status)) {
-        return 1;
-    }
-
-    std::cout << "Seal round trip success! Receive back " << unsealed << std::endl;
-
-    */
 
     return 0;
-
 }
